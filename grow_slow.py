2aad8efa94561eef5bb474c01d22c9c12f31f930
restated <42782756+restated@users.noreply.github.com>
2019-03-22 10:00:50 -0600

    Update grow_slow.py

--- grow_slow.py
+++ grow_slow.py
@@ -15,7 +15,9 @@ ACCESS_SECRET = 'your access secret here'
 # this runs the following script from the command line that takes the photo and saves it
 # it will only work for USB webcams,
 # you'll have to do something different if you're using a pi-cam
-os.system ("fswebcam -d/dev/video0 -r1280x960 --no-banner plantpic.jpg")
+# if you find your webcam can't handle light levels use -S xx to specify skip frames before photo capture
+# capture a second timestamped photo plant-%Y-%m-%d.jpg to start gathering images for your inevitable timelapse
+os.system ("fswebcam -d/dev/video0 -r1280x960 -S 45 --no-banner plantpic.jpg plant-%Y-%m-%d.jpg")
 
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
 photo = open('plantpic.jpg','rb')
