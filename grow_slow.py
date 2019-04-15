0eedb9c7ded9484c053ee639f1fcf24ef398c1b7
restated <42782756+restated@users.noreply.github.com>
2019-04-15 10:04:14 -0600

    Update grow_slow.py

--- grow_slow.py
+++ grow_slow.py
@@ -17,7 +17,7 @@ ACCESS_SECRET = 'your access secret here'
 # you'll have to do something different if you're using a pi-cam
 # if you find your webcam can't handle light levels use -S xx to specify skip frames before photo capture
 # capture a second timestamped photo plant-%Y-%m-%d.jpg to start gathering images for your inevitable timelapse
-os.system ("fswebcam -d/dev/video0 -r1280x960 -S 45 --no-banner plantpic.jpg plant-%Y-%m-%d--%H-%M-%S.jpg")
+os.system ("fswebcam -d/dev/video0 -r1280x960 -S 45 --no-banner plantpic.jpg plant-%Y-%m-%d.jpg")
 
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
 photo = open('plantpic.jpg','rb')
